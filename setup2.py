86faffa29272d5194a0f4813d82a9657c307f6b3
Franz Haas <franz.dominik.haas@gmail.com>
2025-03-20 10:17:56 +0100

    - 2 setup.pys

---
+++ setup2.py
@@ -0,0 +1,17 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {'packages': ["robot", "numpy", "pandas"], 'excludes': []}
+
+base = 'console'
+
+executables = [
+    Executable('robot', base=base, target_name = 'robot_run')
+]
+
+setup(name='robot',
+      version = '1.0',
+      description = '',
+      options = {'build_exe': build_options},
+      executables = executables)
