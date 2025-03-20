0dda793049f4da6da365c5eac9cc1bb39b549686
franz <franz.dominik.haas@gmail.com>
2025-03-20 15:29:07 +0100

    - reenaable linux

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': [], 'excludes': []}
+build_options = {'packages': ["robot"], 'excludes': []}
 
 base = 'console'
 
 executables = [
-    Executable('robot.py', base=base, modules=['robot'])
+    Executable('robot', base=base, target_name = 'robot_run')
 ]
 
-setup(name='r',
+setup(name='robot',
       version = '1.0',
       description = '',
       options = {'build_exe': build_options},
