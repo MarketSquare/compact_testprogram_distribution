91edb375063672a42377b528fd466376567f6540
franz <franz.dominik.haas@gmail.com>
2025-03-20 15:24:10 +0100

    - fixed path

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': ["robot"], 'excludes': []}
+build_options = {'packages': [], 'excludes': []}
 
 base = 'console'
 
 executables = [
-    Executable('robot', base=base, target_name = 'robot_run')
+    Executable('robot.py', base=base, modules=['robot'])
 ]
 
-setup(name='robot',
+setup(name='r',
       version = '1.0',
       description = '',
       options = {'build_exe': build_options},
