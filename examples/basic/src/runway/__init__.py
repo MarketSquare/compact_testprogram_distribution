8a82188fc55970fea84009a41e507249a1fab820
franz haas <franz.dominik.haas@gmail.com>
2025-09-13 15:13:44 +0200

    - added more comments

--- examples/basic/src/runway/__init__.py
+++ examples/basic/src/runway/__init__.py
@@ -11,6 +11,11 @@ def main():
     robot.run_cli()
 
 
-def main2():
+def main_with_packaged_robot_file():
+    """
+    This function demonstrates how to run the robot file included in the package data.
+
+    the packaging of the robot file is configured in setup.cfg
+    """
     with importlib.resources.path(runway.data, "demo.robot") as path:
         robot.run(str(path))
--- examples/numpy/src/runway.py
+++ examples/numpy/src/runway.py
@@ -6,12 +6,33 @@ import sys
 import robot
 import zipimport
 
+
+# This is to improve the speed in python versions 3.10 and 3.11
+# see hhttps://github.com/python/cpython/issues/103200
+#
+# while this does work in this example, it might have side effects
+# in other applications. Use with caution.
+#
+# according to the robotframework maintainer this
+# will lead to bugs in robotframework (which happen to
+# not be relevant for this example)
 zipimport.zipimporter.invalidate_caches = lambda _: None
 
 
 zipapplocation = pathlib.Path(sys.path[0])
 
 if zipapplocation.suffix in {".pyz", ".exe"}:
+    # This is a wild hack which uses temporary directory to make numpy work
+    # in a zipapp. It extracts all files which are in the same top level directory
+    # as a .pyd, .so or .dll file into a temporary directory and adds this
+    # directory to sys.path. This way the dynamic libraries can be found by
+    # numpy.
+    #
+    # This is a hack and might break in the future or has other side effects.
+    #
+    # This is for educational purposes only. Do not use this unless you thouroughly
+    # understand the implications.
+
     dir_tobuildup_dependencies_path = pathlib.Path(tempfile.mkdtemp())
 
     with zipfile.ZipFile(zipapplocation, "r") as z:
